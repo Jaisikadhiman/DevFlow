0a78bae925ee49b73a27a0ab9467699175efe423
Jaisika Dhiman <jaisikadhiman446@gmail.com>
2025-02-19 23:42:27 +0530

    Removed Frontend folder from repository

--- Frontend/connection/dbconnection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-module.exports = dbConnection = () => {
-  mongoose
-    .connect(process.env.DB_URL, {})
-    .then(() => console.log("connected"))
-    .catch((error) => console.log("error" + error));
-};
-// module.exports = dbconnection;
--- Frontend/react_basic/src/components/ProtectedRoutes.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import {Route,Outlet, redirect,Navigate} from "react-router-dom"
-const ProtectedRoutes = () => {
-    const isLoggedIn = localStorage.getItem("token");
-  return isLoggedIn?<Outlet/>:<Navigate to={"login"} />
-}
-
-export default ProtectedRoutes
